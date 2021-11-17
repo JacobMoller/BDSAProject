524efb59624e055478d343d21d631e40b0a019d3
Emma Kirstine Hansen <ekha@itu.dk>
2021-11-17 14:29:18 +0100

    Fills out IProjectRepository

--- ProjectBank/Core/IProjectRepository.cs
+++ ProjectBank/Core/IProjectRepository.cs
@@ -1,5 +1,10 @@
 namespace ProjectBank.Core;
-
-public class IProjectRepository
+public interface IProjectRepository
 {
+    Task<ProjectDTO> CreateProjectAsync(ProjectCreateDTO project);
+    Task<IReadOnlyCollection<ProjectDTO>> ReadAllAsync();
+    Task<ProjectDTO> ReadProjectAsync(int projectId);
+    Task<IReadOnlyCollection<ProjectDTO>> ReadProjectsByUserAsync(int userId);
+    Task<Response> UpdateProjectAsync(ProjectDTO project);
+    Task<Response> DeleteProjectAsync(int projectId);
 }
