96e5ef8ca3cfb32ee2a2a646a4d13237c082f59d
Werenfeldt <63417041+Werenfeldt@users.noreply.github.com>
2021-11-24 09:35:13 +0100

    CreationDate og Update Date removed. Changes from get; init to get;set. Changes the collection of Tags to a collections og strings.

--- ProjectBank/Core/ProjectDTO.cs
+++ ProjectBank/Core/ProjectDTO.cs
@@ -1,35 +1,26 @@
 ﻿namespace ProjectBank.Core;
 
-public record ProjectDTO(int Id, string Title, string? Description, Status Status, int UserId);
+public record ProjectDTO(int Id, string Title, Status Status, int UserId);
 
-public record ProjectDetailsDTO(int Id, string Title, string? Description, Status Status, int UserId, DateTime CreationDate, DateTime UpdatedDate, IReadOnlyCollection<Tag> Tags, IReadOnlyCollection<User> Participants): ProjectDTO(Id, Title, Description, Status, UserId);
+public record ProjectDetailsDTO(int Id, string Title, Status Status, int UserId, string? Description, DateTime CreationDate, DateTime UpdatedDate, IReadOnlyCollection<string> Tags, IReadOnlyCollection<User> Participants): ProjectDTO(Id, Title, Status, UserId);
 
 public record CreateProjectDTO
 {
     [StringLength(50)]
-    public string Title { get; init; }
+    public string Title { get; set; }
 
     [StringLength(500)]
-    public string? Description { get; init; }
+    public string? Description { get; set; }
 
-    public Status Status { get; init; }
+    public int UserId { get; set; }
+    public ICollection<string>? Tags { get; set; }
 
-    public int UserId { get; init; }
-
-    [DataType(DataType.Date)]
-    public DateTime CreationDate { get; init; }
-
-    [DataType(DataType.Date)]
-    public DateTime? UpdatedDate { get; init; }
-    public ICollection<Tag>? Tags { get; init; }
-
-    public ICollection<User>? Participants { get; init; }
-
+    public ICollection<User>? Participants { get; set; }
 }
 
 public record UpdateProjectDTO : CreateProjectDTO
 {
-    public int Id { get; init; }
+    public int Id { get; set; }
 }
 
 
