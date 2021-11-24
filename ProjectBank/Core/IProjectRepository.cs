be81c9ddd27dd4aa70113711500aa94b1c2889cc
EmmKirHan <75368461+EmmKirHan@users.noreply.github.com>
2021-11-24 16:44:14 +0100

    Fills out IProjectRepository (#148)
    * Docker and small test data
    
    * Docker, Migrations og Database
    
    * Solutionfile
    
    * added razor files
    
    * with razor files
    
    * small changes in razor imports.
    
    * Docker og Blazer snakker sammen. DONE
    
    * Up to date with All pull requests: #125, #128, #132, #139, #145, #146
    
    * Fills out IProjectRepository
    
    * Adds ReadByTag and UpdateStatus methods
    
    * removed files
    
    * removed file
    
    * removed more files
    
    * removed more files
    
    * removed files
    
    * Update ProjectBank/Core/IProjectRepository.cs
    
    Co-authored-by: Jacob Møller <jmj1999@gmail.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Jacob Møller <jmj1999@gmail.com>
    
    * removed Response as return value
    
    Co-authored-by: Werenfeldt <63417041+Werenfeldt@users.noreply.github.com>
    Co-authored-by: Emma Kirstine Hansen <ekha@itu.dk>
    Co-authored-by: jeppekorgaard <korg@itu.dk>
    Co-authored-by: JeppeKorgaard98 <90270219+JeppeKorgaard98@users.noreply.github.com>
    Co-authored-by: Jacob Møller <jmj1999@gmail.com>

--- ProjectBank/Core/IProjectRepository.cs
+++ ProjectBank/Core/IProjectRepository.cs
@@ -1,5 +1,12 @@
 namespace ProjectBank.Core;
-
-public class IProjectRepository
+public interface IProjectRepository
 {
+    Task<ProjectDTO> CreateProjectAsync(ProjectCreateDTO project);
+    Task<IReadOnlyCollection<ProjectDTO>> ReadAllAsync();
+    Task<ProjectDTO> ReadProjectByIdAsync(int projectId);
+    Task<IReadOnlyCollection<ProjectDTO>> ReadProjectsByUserAsync(int userId);
+    Task<IReadOnlyCollection<ProjectDTO>> ReadProjectsByTagAsync(int tagId);
+    void EditProjectAsync(ProjectDTO project);
+    void UpdateProjectStatusByIdAsync(int projectId);
+    void DeleteProjectByIdAsync(int projectId);
 }
