706db353ed063d5abdbc4fa8568474c4a1a83abf
Werenfeldt <63417041+Werenfeldt@users.noreply.github.com>
2021-11-24 12:12:24 +0100

    ProjectDTO Implemented

--- ProjectBank/Core/ProjectDTO.cs
+++ ProjectBank/Core/ProjectDTO.cs
@@ -1,6 +1,24 @@
 ﻿namespace ProjectBank.Core;
 
-public class ProjectDTO
+public record ProjectDTO(int Id, string Title, Status Status, int UserId);
+
+public record ProjectDetailsDTO(int Id, string Title, Status Status, int UserId, string? Description, DateTime CreationDate, DateTime UpdatedDate, IReadOnlyCollection<string> Tags, IReadOnlyCollection<User> Participants): ProjectDTO(Id, Title, Status, UserId);
+
+public record CreateProjectDTO
 {
+    [StringLength(50)]
+    public string Title { get; set; }
+
+    [StringLength(500)]
+    public string? Description { get; set; }
+
+    public int UserId { get; set; }
+    public ICollection<string>? Tags { get; set; }
+
+    public ICollection<User>? Participants { get; set; }
 }
 
+public record UpdateProjectDTO : CreateProjectDTO
+{
+    public int Id { get; set; }
+}
