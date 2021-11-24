aea5680e92b381f1e9dea9f56a6a0cd96659ad6c
JeppeKorgaard98 <90270219+JeppeKorgaard98@users.noreply.github.com>
2021-11-24 16:30:41 +0100

    Apply suggestions from code review
    Co-authored-by: Jacob Møller <jmj1999@gmail.com>

--- ProjectBank/Core/IProjectRepository.cs
+++ ProjectBank/Core/IProjectRepository.cs
@@ -5,8 +5,8 @@ public interface IProjectRepository
     Task<IReadOnlyCollection<ProjectDTO>> ReadAllAsync();
     Task<ProjectDTO> ReadProjectByIdAsync(int projectId);
     Task<IReadOnlyCollection<ProjectDTO>> ReadProjectsByUserAsync(int userId);
-    Task<IReadOnlyCollection<ProjectDTO>> ReadProjectsByTag(int tagId);
+    Task<IReadOnlyCollection<ProjectDTO>> ReadProjectsByTagAsync(int tagId);
     Task<Response> EditProjectAsync(ProjectDTO project);
-    Task<Response> UpdateProjectStatusAsync(int projectId);
-    Task<Response> DeleteProjectAsync(int projectId);
+    Task<Response> UpdateProjectStatusByIdAsync(int projectId);
+    Task<Response> DeleteProjectByIdAsync(int projectId);
 }
