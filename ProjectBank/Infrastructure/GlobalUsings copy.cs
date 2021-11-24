470ca133575130e3db2f6891850e6e028b68ca79
Jacob Møller <jmj1999@gmail.com>
2021-11-24 13:44:29 +0100

    Delete GlobalUsings copy.cs

--- ProjectBank/Infrastructure/GlobalUsings copy.cs
+++
@@ -1,4 +0,0 @@
-global using System.ComponentModel.DataAnnotations;
-global using Microsoft.EntityFrameworkCore;
-global using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
-global using ProjectBank.Core;
