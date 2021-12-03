336e581da95fe02ad4daaffe890841e30ee80bb6
frev <frev@itu.dk>
2021-12-03 14:43:57 +0100

    Fix assertion mistakes

--- ProjectBank/Infrastructure.Tests/ProjectRepositoryTests.cs
+++ ProjectBank/Infrastructure.Tests/ProjectRepositoryTests.cs
@@ -58,14 +58,14 @@ public class ProjectRepositoryTests : ContextSetup, IDisposable
     {
         //maybe we should make the dto separately to test with it's values instead of hardcoding?
         await _projectRepository.CreateProjectAsync(new CreateProjectDTO { Title = "Algo", UserId = 1, Description = "Very fun", Tags = new List<string> { "Sorting" } });
-        await _projectRepository.EditProjectAsync(new UpdateProjectDTO { Id = 1, Tags = new List<string> { "Sorting", "Economy", "Math" } });
+        await _projectRepository.EditProjectAsync(new UpdateProjectDTO { Id = 1, Title = "Algo", UserId = 1, Description = "Very fun", Tags = new List<string> { "Sorting", "Economy", "Math" } });
         var editedProjectWithTags = await _context.Projects.FindAsync(1);
 
         Assert.Equal(1, editedProjectWithTags.Id);
         Assert.Equal("Algo", editedProjectWithTags.Title);
         Assert.Equal(Status.Active, editedProjectWithTags.Status);
         Assert.Equal(1, editedProjectWithTags.UserId);
-        Assert.Equal("Not sorting", editedProjectWithTags.Description);
+        Assert.Equal("Very fun", editedProjectWithTags.Description);
         Assert.Equal(DateTime.UtcNow, editedProjectWithTags.UpdatedDate, precision: TimeSpan.FromSeconds(5));
         Assert.Equal(new List<string>() { "Sorting", "Economy", "Math" }, editedProjectWithTags.Tags.Select(tag => new string(tag.Name)).ToList());
     }
@@ -74,13 +74,13 @@ public class ProjectRepositoryTests : ContextSetup, IDisposable
     public async Task EditProject_given_UpdateProjectDTO_updates_project_removes_tags()
     {
         await _projectRepository.CreateProjectAsync(new CreateProjectDTO { Title = "DMAT", UserId = 2, Description = "Very very fun", Tags = new List<string> { "Math" } });
-        await _projectRepository.EditProjectAsync(new UpdateProjectDTO { Id = 2, Title = "DMAT 2", UserId = 2, Description = "Very fun" });
-        var editedProjectWithoutTags = await _context.Projects.FindAsync(2);
+        await _projectRepository.EditProjectAsync(new UpdateProjectDTO { Id = 1, Title = "DMAT 2", UserId = 2, Description = "Very fun" });
+        var editedProjectWithoutTags = await _context.Projects.FindAsync(1);
 
         Assert.Equal(1, editedProjectWithoutTags.Id);
         Assert.Equal("DMAT 2", editedProjectWithoutTags.Title);
         Assert.Equal(Status.Active, editedProjectWithoutTags.Status);
-        Assert.Equal(1, editedProjectWithoutTags.UserId);
+        Assert.Equal(2, editedProjectWithoutTags.UserId);
         Assert.Equal("Very fun", editedProjectWithoutTags.Description);
         Assert.Equal(DateTime.UtcNow, editedProjectWithoutTags.UpdatedDate, precision: TimeSpan.FromSeconds(5));
         Assert.Equal(new List<string>(), editedProjectWithoutTags.Tags.Select(tag => new string(tag.Name)).ToList());
