6903629a500a6f26df62b784a47a48fdd09ea9fb
JacobM <jacj@itu.dk>
2021-12-07 12:34:19 +0100

    Connect ExploreProjectsPage with DB

---
+++ ProjectBank/Server/Controllers/ProjectsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectBank.Shared;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Identity.Web.Resource;
+
+namespace ProjectBank.Server.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+[RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
+public class ProjectsController : ControllerBase
+{
+    private IProjectRepository _projectRepository;
+
+    public ProjectsController(IProjectRepository repo)
+    {
+        _projectRepository = repo;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<ProjectDTO>> Get()
+    {
+        return await _projectRepository.ReadAllAsync();
+    }
+}
+
