48dfae59e705e7e2f3f3fd2f9d39f3f0ec543ac2
frev <frev@itu.dk>
2021-12-09 16:50:03 +0100

    Working create project

--- ProjectBank/Client/GlobalUsings.cs
+++ ProjectBank/Client/GlobalUsings.cs
@@ -1,5 +1,6 @@
 global using Microsoft.AspNetCore.Components.Web;
 global using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using System.Text.RegularExpressions;
 
 global using ProjectBank.Client;
